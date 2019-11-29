43c03f98c819dc5cd75d8b661671a3921e790fa7
inwonkim <inwonkim@icon.foundation>
2019-11-29 17:28:44 +0900

    Write account querying command and precommit querying command
    * precommit can be queried with blockHeight or address
    * accountDB can be queried in 4  ways
      - with path option
      - with path, address option
      - with rcRoot option, accountType option
      - with rcRoot option, accountType, address  option

--- cmd/dbtool/db_account.go
+++ cmd/dbtool/db_account.go
@@ -5,61 +5,36 @@ import (
 	"github.com/icon-project/rewardcalculator/common"
 	"github.com/icon-project/rewardcalculator/common/db"
 	"github.com/icon-project/rewardcalculator/core"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func queryAccountDB(input Input) {
-	if input.path == "" && input.address == "" {
-		fmt.Println("address or dbPath required")
-		os.Exit(1)
-	}
 	if input.path != "" {
-		dir, name := filepath.Split(input.path)
-		qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
-		defer qdb.Close()
-
-		iteratePrintDB(DataTypeAccount, qdb)
-	} else {
+		queryAccountWithAccountDBPath(input.path, input.address)
+		return
+	} else if !(input.rcDBRoot == "" && input.accountType == "") {
 		// get db info
-		if input.rcDBRoot == "" {
-			fmt.Printf("rcPath required when querying with address")
-			os.Exit(1)
-		}
-		dbInfo := getDBInfo(input.rcDBRoot)
-		addr := common.NewAddressFromString(input.address)
-		accountDBPath := getAccountDBPath(input.rcDBRoot, addr, dbInfo)
-		dir, name := filepath.Split(accountDBPath)
-		qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
-		defer qdb.Close()
-
-		bucket, err := qdb.GetBucket(db.PrefixIScore)
-		if err != nil {
-			fmt.Printf("Failed to get bucket")
-			return
-		}
-
-		value, err := bucket.Get(addr.Bytes())
-		if value == nil || err != nil {
-			fmt.Printf("failed to get value in DB")
-			return
-		}
-
-		printAccount(addr.Bytes(), value, addr)
+		queryAccountWithRCRootPath(input.rcDBRoot, input.address, input.accountType)
+		return
 	}
+	fmt.Println("DBPath or (rcDBPath and accountType) required")
+	os.Exit(1)
 }
 
 func printAccount(key []byte, value []byte, address *common.Address) bool {
 	account, err := core.NewIScoreAccountFromBytes(value)
 	if err != nil {
-		log.Printf("Failed to make claim instance")
-		fmt.Println("printAccount1")
+		log.Printf("Failed to make account instance")
 		return false
 	}
 	account.Address = *common.NewAddress(key)
 
 	if address != nil && account.Address.Equal(address) == false {
+
 		return false
 	}
 	fmt.Printf("%s\n", account.String())
@@ -67,38 +42,170 @@ func printAccount(key []byte, value []byte, address *common.Address) bool {
 	return true
 }
 
-func getDBInfo(dbRoot string) *core.DBInfo {
-	var dbInfo *core.DBInfo
-	mdb := db.Open(dbRoot, string(db.GoLevelDBBackend), "")
-	bucket, err := mdb.GetBucket(db.PrefixManagement)
-	dbInfo = new(core.DBInfo)
-	data, err := bucket.Get(dbInfo.ID())
-	if data != nil {
-		err = dbInfo.SetBytes(data)
-		if err != nil {
-			fmt.Println("Failed to set DB Information structure")
-			os.Exit(1)
-		}
-	} else {
-		fmt.Println("Invalid IScore DB")
-		os.Exit(1)
+func queryAccountWithAccountDBPath(path string, address string) {
+	dir, name := filepath.Split(path)
+	qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
+	defer qdb.Close()
+	if address == "" {
+		iteratePrintDB(DataTypeAccount, qdb)
+		return
 	}
-	return dbInfo
+	addr := common.NewAddressFromString(address)
+	bucket, err := qdb.GetBucket(db.PrefixIScore)
+	if err != nil {
+		fmt.Printf("Failed to get bucket")
+		return
+	}
+
+	value, err := bucket.Get(addr.Bytes())
+	if value == nil || err != nil {
+		fmt.Printf("failed to get value in DB")
+		return
+	}
+	printAccount(addr.Bytes(), value, addr)
 }
 
-func getAccountDBPath(dbRoot string, address *common.Address, dbInfo *core.DBInfo) string {
-	if address == nil || dbInfo == nil {
-		fmt.Println("Can not get AccountDB without Address and DBInfo")
+func queryAccountWithRCRootPath(path string, address string, accountType string) {
+	if accountType != "query" && accountType != "calculate" {
+		fmt.Println("invalid accountType")
 		os.Exit(1)
 	}
-	prefix := int(address.ID()[0]) % dbInfo.DBCount
-	index := prefix + 1
-	if dbInfo.QueryDBIsZero {
-		accountDBPath := fmt.Sprintf(core.AccountDBNameFormat, index, dbInfo.DBCount, 0)
-		dbRoot = filepath.Join(dbRoot, accountDBPath)
-	} else {
-		accountDBPath := fmt.Sprintf(core.AccountDBNameFormat, index, dbInfo.DBCount, 1)
-		dbRoot = filepath.Join(dbRoot, accountDBPath)
+	accountDBNum := getAccountDBCount(path)
+	if address != "" {
+		addr := common.NewAddressFromString(address)
+		prefix := int(addr.ID()[0]) % accountDBNum
+		index := prefix + 1
+		accountDBPath0 := fmt.Sprintf(core.AccountDBNameFormat, index, accountDBNum, 0)
+		dbRoot0 := filepath.Join(path, accountDBPath0)
+		account0 := getAccount(dbRoot0, addr)
+		accountDBPath1 := fmt.Sprintf(core.AccountDBNameFormat, index, accountDBNum, 1)
+		dbRoot1 := filepath.Join(path, accountDBPath1)
+		account1 := getAccount(dbRoot1, addr)
+		var calculateAccount *core.IScoreAccount
+		var queryAccount *core.IScoreAccount
+		if account0 != nil {
+			if account0.BlockHeight >= account1.BlockHeight {
+				calculateAccount = account0
+				queryAccount = account1
+			} else {
+				calculateAccount = account1
+				queryAccount = account0
+			}
+			if accountType == "query" {
+				fmt.Println("account : ", queryAccount.String())
+			} else {
+				fmt.Println("account : ", calculateAccount.String())
+			}
+		}
+		return
 	}
-	return dbRoot
+
+	dbIndex := 0
+	var accountDBPath string
+	found := false
+	for i := 1; i <= accountDBNum; i++ {
+		if !found {
+			accountDBPath0 := fmt.Sprintf(core.AccountDBNameFormat, i, accountDBNum, 0)
+			dbPath0 := filepath.Join(path, accountDBPath0)
+			accounts0 := getAccounts(dbPath0)
+			accountDBPath1 := fmt.Sprintf(core.AccountDBNameFormat, i, accountDBNum, 1)
+			dbPath1 := filepath.Join(path, accountDBPath1)
+			accounts1 := getAccounts(dbPath1)
+			if len(accounts0) > 0 {
+				found = true
+				if accounts0[0].BlockHeight >= accounts1[0].BlockHeight {
+					if accountType == "query" {
+						dbIndex = 1
+						accountDBPath = accountDBPath1
+					} else {
+						dbIndex = 0
+						accountDBPath = accountDBPath0
+					}
+				} else {
+					if accountType == "query" {
+						dbIndex = 0
+						accountDBPath = accountDBPath0
+					} else {
+						dbIndex = 1
+						accountDBPath = accountDBPath1
+					}
+				}
+			}
+
+		}
+		accountDBPath = fmt.Sprintf(core.AccountDBNameFormat, i, accountDBNum, dbIndex)
+		dbPath := filepath.Join(path, accountDBPath)
+		fmt.Println(dbPath)
+		printAccountsInDB(dbPath)
+	}
+
+}
+func getAccountDBCount(path string) int {
+	contents, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println("Failed to read directory")
+		os.Exit(1)
+	}
+	count := 0
+	for _, f := range contents {
+		if strings.HasPrefix(f.Name(), "calculate_") {
+			count++
+		}
+	}
+	result := count / 2
+	return result
+}
+
+func getAccount(dbPath string, address *common.Address) *core.IScoreAccount {
+	dir, name := filepath.Split(dbPath)
+	qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
+	defer qdb.Close()
+	bucket, err := qdb.GetBucket(db.PrefixIScore)
+	if err != nil {
+		fmt.Printf("Failed to get account bucket")
+		os.Exit(1)
+	}
+
+	value, err := bucket.Get(address.Bytes())
+	if err != nil {
+		fmt.Printf("error while get value in DB")
+		os.Exit(1)
+	}
+	if value == nil {
+		fmt.Printf("failed to get value in DB")
+		return nil
+	}
+	account, err := core.NewIScoreAccountFromBytes(value)
+	account.Address = *address
+	return account
+}
+
+func getAccounts(dbPath string) []*core.IScoreAccount {
+	dir, name := filepath.Split(dbPath)
+	qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
+	defer qdb.Close()
+	accounts := []*core.IScoreAccount{}
+	iter, err := qdb.GetIterator()
+	if err != nil {
+
+	}
+	prefix := getPrefix(DataTypeAccount)
+	iter.New(prefix.Start, prefix.Limit)
+	for iter.Next() {
+		account, err := core.NewIScoreAccountFromBytes(iter.Value())
+		if err != nil {
+			fmt.Println("Error while init IscoreAccount")
+			os.Exit(1)
+		}
+		accounts = append(accounts, account)
+	}
+	iter.Release()
+	return accounts
+}
+
+func printAccountsInDB(dbPath string) {
+	dir, name := filepath.Split(dbPath)
+	qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
+	defer qdb.Close()
+	iteratePrintDB(DataTypeAccount, qdb)
 }
--- cmd/dbtool/db_precommit.go
+++ cmd/dbtool/db_precommit.go
@@ -20,42 +20,13 @@ func queryPreCommitDB(input Input) {
 	qdb := db.Open(dir, string(db.GoLevelDBBackend), name)
 	defer qdb.Close()
 
-	if input.address == "" || input.height == 0 {
+	if input.address == "" && input.height == 0 {
 		iteratePrintDB(DataTypePreCommit, qdb)
 		return
 	}
 
-	iter, err := qdb.GetIterator()
-	if err != nil {
-		fmt.Printf("Failed to get iterator")
-		return
-	}
-
 	addr := common.NewAddressFromString(input.address)
-
-	iter.New(nil, nil)
-	preCommitKey := make([]byte, core.BlockHashSize+core.BlockHeightSize+common.AddressBytes)
-	keyExist := false
-	blockHeightBytesValue := common.Uint64ToBytes(input.height)
-	for iter.Next() {
-		key := iter.Key()
-		if bytes.Equal(key[:core.BlockHeightSize], blockHeightBytesValue) &&
-			bytes.Equal(key[core.BlockHeightSize+core.BlockHashSize:], addr.Bytes()) {
-			keyExist = true
-			copy(preCommitKey, key)
-		}
-	}
-	iter.Release()
-
-	if keyExist == false {
-		fmt.Printf("Can not find key using given information")
-		return
-	}
-	err = iter.Error()
-	if err != nil {
-		fmt.Printf("Error while iterate")
-		return
-	}
+	qPreCommitKeys := getKeys(qdb, *addr, input.height)
 
 	bucket, err := qdb.GetBucket(db.PrefixClaim)
 	if err != nil {
@@ -63,11 +34,13 @@ func queryPreCommitDB(input Input) {
 		return
 	}
 
-	value, err := bucket.Get(preCommitKey)
-	if value == nil || err != nil {
-		return
+	for _, key := range qPreCommitKeys {
+		value, err := bucket.Get(key)
+		if value == nil || err != nil {
+			continue
+		}
+		printPreCommit(key, value, addr, input.height)
 	}
-	printPreCommit(preCommitKey, value, addr, input.height)
 }
 
 func printPreCommit(key []byte, value []byte, address *common.Address, blockHeight uint64) bool {
@@ -94,4 +67,51 @@ func printPreCommit(key []byte, value []byte, address *common.Address, blockHeig
 	fmt.Printf("%s\n", pc.String())
 
 	return true
+}
+
+func getKeys(qdb db.Database, address common.Address, blockHeight uint64) [][]byte{
+	iter, err := qdb.GetIterator()
+	if err != nil {
+		fmt.Printf("Failed to get precommit db iterator")
+		os.Exit(1)
+	}
+
+	precommitKeys := [][]byte{}
+	iter.New(nil, nil)
+	keyExist := false
+	tmpAddress := new(common.Address)
+	blockHeightBytesValue := common.Uint64ToBytes(blockHeight)
+	for iter.Next() {
+		key := iter.Key()
+		if address.Equal(tmpAddress) == false && blockHeight != 0 {
+			if bytes.Equal(key[:core.BlockHeightSize], blockHeightBytesValue) &&
+				bytes.Equal(key[core.BlockHeightSize+core.BlockHashSize:], address.Bytes()) {
+				keyExist = true
+				precommitKeys = append(precommitKeys, key)
+				break
+			}
+		}else{
+			if address.Equal(tmpAddress) == false &&
+				bytes.Equal(key[core.BlockHeightSize+core.BlockHashSize:], address.Bytes()){
+				keyExist = true
+				precommitKeys = append(precommitKeys, key)
+			} else if blockHeight != 0 && bytes.Equal(key[:core.BlockHeightSize], blockHeightBytesValue){
+				keyExist = true
+				precommitKeys = append(precommitKeys, key)
+			}
+		}
+	}
+	iter.Release()
+
+	if keyExist == false {
+		fmt.Printf("Can not find key using given information")
+		os.Exit(1)
+	}
+	err = iter.Error()
+	if err != nil {
+		fmt.Printf("Error while iterate")
+		os.Exit(1)
+	}
+
+	return precommitKeys
 }
